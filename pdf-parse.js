318e18277174c3d1052e80b23b8f65f8b1d78b0a
Jan Ilves <janilves1212@gmail.com>
2025-03-07 14:58:32 +0200

    Throws errors when the statement doesn't follow standad format

--- pdf-parse.js
+++ pdf-parse.js
@@ -11,15 +11,22 @@ function getDateIndex
     if (parseFloat(xposition) > 300 && parseFloat(xposition) < 364) return 0;
 }
 
+class MyError extends Error{
+    constructor(message){
+        super(message);
+    }
+}
+
+
 function parsePage(items) {
     let reportname;
     let date;
     let aruanne = [];
-    let isTabel = Boolean;
+    let isTabel = 0;
     const companyname = items[1].str;
     for (let item of items) {
         if (item.str && item.str !== " ") {
-         if ((item.str === "Bilanss" || item.str === "Kasumiaruanne") && parseInt(item.height) == 14) {
+            if ((item.str === "Bilanss" || item.str === "Kasumiaruanne") && parseInt(item.height) == 14) {
                 isTabel = true;
                 reportname = item.str;
             }
@@ -31,12 +38,12 @@ function parsePage(items) {
                 }
                 else if (date) {
                     if (parseFloat(item.transform[4]) < 300) {
-                        let obj = { [item.str]: ""};
+                        let obj = { [item.str]: "" };
                         aruanne.push(obj);
                     }
                     else if (parseFloat(item.transform[4]) > 300 && parseFloat(item.transform[4]) < 364) {
                         let key = Object.keys(aruanne[aruanne.length - 1])[0];
-                        let item_str = item.str;    
+                        let item_str = item.str;
                         //console.log(Object.keys(key));
                         aruanne[aruanne.length - 1][key] = item_str;
 
@@ -50,11 +57,13 @@ function parsePage(items) {
     }
 
 
-    
-    isTabel = false;
+    if (!isTabel) {
+        throw new MyError("The document is in unfamiliar format to the system");
+        //return 
+    }
     return report = {
-        companyname : companyname,
-        date : date,
+        companyname: companyname,
+        date: date,
         [reportname]: aruanne
     };
 
@@ -62,7 +71,7 @@ function parsePage(items) {
 
 
 
-async function render_page(pageData) {
+async function render_page(pageData, file, page) {
 
     //check documents https://mozilla.github.io/pdf.js/
     let render_options = {
@@ -75,22 +84,54 @@ async function render_page(pageData) {
     }
 
     let textContent = await pageData.getTextContent(render_options);
-    return parsePage(textContent.items);
+    try {
+        return parsePage(textContent.items);
+    }
+    catch(error){
+        console.log(file, "Page" + page, error.message)
+}
+
 
 }
 
 
+function parsePDF(dataBuffer, file) {
 
 let options = {
     pagerender: async function (pageData) {
         const page = (pageData.pageIndex) + 1;
         if (page == 4 || page == 5) {
-            let pageRendered = await render_page(pageData);
-            console.log(JSON.stringify(pageRendered
-                , null, 2));
-        }
+            pdfDataExtracted
+                .push(await render_page(pageData, file, page));
+
+            }
+            
 
     }
+
+}
+
+pdf(dataBuffer, options).then(function (data) {
+           
+    // console.log(JSON.stringify(pdfDataExtracted
+    // , null, 2));
+
+
+    // // number of pages
+    // console.log(data.numpages);
+    // // number of rendered pages
+    //console.log(data.numrender);
+    // // PDF info
+    // console.log(data.info);
+    // // PDF metadata
+    // console.log(data.metadata);
+    // // PDF.js version
+    // // check https://mozilla.github.io/pdf.js/getting_started/
+    // console.log(data.version);
+    // PDF text
+    // console.log(data.text);
+
+})
 
 }
 
@@ -99,26 +140,10 @@ let options = {
 
 fs.readdirSync(dataFolder).forEach(file => {
     let dataBuffer = fs.readFileSync(dataFolder + "/" + file)
-    pdf(dataBuffer, options).then(function (data) {
-
-
-        // // number of pages
-        // console.log(data.numpages);
-        // // number of rendered pages
-        //console.log(data.numrender);
-        // // PDF info
-        // console.log(data.info);
-        // // PDF metadata
-        // console.log(data.metadata);
-        // // PDF.js version
-        // // check https://mozilla.github.io/pdf.js/getting_started/
-        // console.log(data.version);
-        // PDF text
-        // console.log(data.text);
-
-    });
-
-}
+    console.log("Parsing file " + file);
+    parsePDF(dataBuffer, file);
+  }
+    
 );
 
 
