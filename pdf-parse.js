9121b4db047d703b88ef539e12c1ad0ce0029444
Jan Ilves <janilves1212@gmail.com>
2025-03-09 17:55:36 +0200

    Improved code and added translation module

--- pdf-parse.js
+++ pdf-parse.js
@@ -1,34 +1,24 @@
 const fs = require('fs');
 const pdf = require('pdf-parse');
+const eng = require('./eng.json');
 
-let pdfDataExtracted = [];
+
 const dataFolder = "./uploads";
 
 
-function getDateIndex
-    (xposition) {
-    if (parseFloat(xposition) > 364 && parseFloat(xposition) < 426) return 1;
-    if (parseFloat(xposition) > 300 && parseFloat(xposition) < 364) return 0;
-}
-
-class MyError extends Error{
-    constructor(message){
-        super(message);
-    }
-}
-
 
 function parsePage(items) {
+    let objectKey;
     let reportname;
     let date;
     let aruanne = [];
     let isTabel = 0;
     const companyname = items[1].str;
-    for (let item of items) {
+    for (const [index, item] of items.entries()) {
         if (item.str && item.str !== " ") {
             if ((item.str === "Bilanss" || item.str === "Kasumiaruanne") && parseInt(item.height) == 14) {
                 isTabel = true;
-                reportname = item.str;
+                reportname = eng[item.str];
             }
             if (isTabel == 1) {
                 if (!date) {
@@ -38,27 +28,27 @@ function parsePage(items) {
                 }
                 else if (date) {
                     if (parseFloat(item.transform[4]) < 300) {
-                        let obj = { [item.str]: "" };
-                        aruanne.push(obj);
+                        objectKey = eng[item.str];
                     }
                     else if (parseFloat(item.transform[4]) > 300 && parseFloat(item.transform[4]) < 364) {
-                        let key = Object.keys(aruanne[aruanne.length - 1])[0];
-                        let item_str = item.str;
-                        //console.log(Object.keys(key));
-                        aruanne[aruanne.length - 1][key] = item_str;
+                        aruanne.push({
+                            [objectKey] : [item.str]
+                        })
 
                     }
                 }
 
             }
-            //console.log(item.transform);
+        }
+        if (index == 5 && isTabel == 0) {
+            break;
         }
 
     }
 
 
     if (!isTabel) {
-        throw new MyError("The document is in unfamiliar format to the system");
+        throw new Error("The document is in unfamiliar format to the system");
         //return 
     }
     return report = {
@@ -74,7 +64,7 @@ function parsePage(items) {
 async function render_page(pageData, file, page) {
 
     //check documents https://mozilla.github.io/pdf.js/
-    let render_options = {
+    const render_options = {
         //replaces all occurrences of whitespace with standard spaces (0x20). The default value is `false`.
         normalizeWhitespace: false,
         //do not attempt to combine same line TextItem's. The default value is `false`.
@@ -83,13 +73,13 @@ async function render_page(pageData, file, page) {
 
     }
 
-    let textContent = await pageData.getTextContent(render_options);
+    const textContent = await pageData.getTextContent(render_options);
     try {
         return parsePage(textContent.items);
     }
-    catch(error){
-        console.log(file, "Page" + page, error.message)
-}
+    catch (err) {
+        console.error(file, "Page" + page, err.message);
+    }
 
 
 }
@@ -97,41 +87,50 @@ async function render_page(pageData, file, page) {
 
 function parsePDF(dataBuffer, file) {
 
-let options = {
-    pagerender: async function (pageData) {
-        const page = (pageData.pageIndex) + 1;
-        if (page == 4 || page == 5) {
-            pdfDataExtracted
-                .push(await render_page(pageData, file, page));
+    const pdfDataExtracted = [];
 
+    const options = {
+        pagerender: async function (pageData) {
+            const page = (pageData.pageIndex) + 1;
+            if (page == 4 || page == 5) {
+                    pdfDataExtracted
+                        .push(await render_page(pageData, file, page));
             }
-            
+
+
+        }
 
     }
 
-}
-
-pdf(dataBuffer, options).then(function (data) {
-           
-    // console.log(JSON.stringify(pdfDataExtracted
-    // , null, 2));
 
 
-    // // number of pages
-    // console.log(data.numpages);
-    // // number of rendered pages
-    //console.log(data.numrender);
-    // // PDF info
-    // console.log(data.info);
-    // // PDF metadata
-    // console.log(data.metadata);
-    // // PDF.js version
-    // // check https://mozilla.github.io/pdf.js/getting_started/
-    // console.log(data.version);
-    // PDF text
-    // console.log(data.text);
+        pdf(dataBuffer, options).then(function (data) {
+            console.log(JSON.stringify(pdfDataExtracted
+                , null, 2));
 
-})
+
+            // console.log(JSON.stringify(pdfDataExtracted
+            // , null, 2));
+
+
+            // // number of pages
+            // console.log(data.numpages);
+            // // number of rendered pages
+            //console.log(data.numrender);
+            // // PDF info
+            // console.log(data.info);
+            // // PDF metadata
+            // console.log(data.metadata);
+            // // PDF.js version
+            // // check https://mozilla.github.io/pdf.js/getting_started/
+            // console.log(data.version);
+            // PDF text
+            // console.log(data.text);
+
+        })
+
+
+    
 
 }
 
@@ -140,10 +139,9 @@ pdf(dataBuffer, options).then(function (data) {
 
 fs.readdirSync(dataFolder).forEach(file => {
     let dataBuffer = fs.readFileSync(dataFolder + "/" + file)
-    console.log("Parsing file " + file);
     parsePDF(dataBuffer, file);
-  }
-    
+}
+
 );
 
 
